e24702c0352d8845a0076e8e76514633a7607c88
whysw <35984562+YangSeungWon@users.noreply.github.com>
2021-12-13 03:01:48 +0900

    use only gyro data

--- videoPlayer/Assets/Classifier/Scripts/Classifier.cs
+++ videoPlayer/Assets/Classifier/Scripts/Classifier.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class Classifier : MonoBehaviour
@@ -24,13 +25,15 @@ public class Classifier : MonoBehaviour
 
     private TensorFlowLite.Interpreter Interpreter;
 
-    float[,] inputs = new float[396, 6];
+    float[,] inputs = new float[396, 3];
 
     float[] outputs = new float[6];
 
     // Start is called before the first frame update
     void Start()
     {
+        Input.gyro.enabled = true;
+
         byte[] modelData = TensorFlowLite.FileUtil.LoadFile(modelPath);
 
         var options = new TensorFlowLite.InterpreterOptions()
@@ -44,41 +47,60 @@ public class Classifier : MonoBehaviour
         Interpreter.AllocateTensors();
     }
 
+    Vector3 QtoV(Quaternion q)
+    {
+        var w = q.w;
+        var x = q.x;
+        var y = q.y;
+        var z = q.z;
+        var roll = Mathf.Atan2(2 * y * w - 2 * x * z, 1 - 2 * y * y - 2 * z * z);
+        var pitch = Mathf.Atan2(2 * x * w - 2 * y * z, 1 - 2 * x * x - 2 * z * z);
+        var yaw = Mathf.Asin(2 * x * y + 2 * z * w);
+        return new Vector3(roll, pitch, yaw);
+    }
+
     void CollectData()
     {
-        var _acc = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
-        var _gyr = Input.gyro.rotationRate;
-        var _gra = Input.gyro.gravity;
-        var _lin = _acc - _gra;
+        // var _acc = new Vector3(Input.acceleration.x, Input.acceleration.y, Input.acceleration.z);
+        var _gyrq = Input.gyro.attitude;
 
-        acc.AddLast(_acc);
-        gra.AddLast(_gra);
-        gyr.AddLast(_gyr);
-        lin.AddLast(_lin);
+        // Debug.Log($"gyro data raw : {string.Join(",", _gyrq)}");
+        var _gyr = QtoV(_gyrq);
 
-        if (steps >= max_steps)
+        // Debug.Log($"gyro data : {string.Join(",", _gyr)}");
+        // var _gra = Input.gyro.gravity;
+        // var _lin = _acc - _gra;
+        int i, j;
+        for (i = 0; i < 6; i++)
         {
-            acc.RemoveFirst();
-            gra.RemoveFirst();
-            gyr.RemoveFirst();
-            lin.RemoveFirst();
+            // acc.AddLast(_acc);
+            // gra.AddLast(_gra);
+            gyr.AddLast(_gyr);
+            // lin.AddLast(_lin);
+            steps++;    
         }
-        else steps++;
+
+        while (steps > max_steps)
+        {
+            // acc.RemoveFirst();
+            // gra.RemoveFirst();
+            gyr.RemoveFirst();
+            // lin.RemoveFirst();
+            steps--;
+        }
 
         // map sensor
-        int i = 0, j = 0;
-        foreach(var collection in new List<LinkedList<Vector3>>() { acc, gyr })
+        i = 0;
+        j = 0;
+        foreach (var collection in new List<LinkedList<Vector3>>() { gyr })
         {
             i = 0;
-            foreach(var entry in collection)
+            foreach (var entry in collection)
             {
                 inputs[i, j + 0] = entry.x;
-
                 inputs[i, j + 1] = entry.y;
-
                 inputs[i, j + 2] = entry.z;
-                
-                i++;
+                i += 1;
             }
             j += 3;
         }
@@ -90,12 +112,38 @@ public class Classifier : MonoBehaviour
         Interpreter.GetOutputTensorData(0, outputs);
 
         var _out = new List<float>(outputs);
-
         Debug.Log($"prediction : {string.Join(",", _out)}");
+        var prediction = 0;
+        if (_out[0] > 0.99)
+            prediction = 0;
+        else
+        {
+            _out[0] = 0;
+            var maxvalue = _out.Max();
+            if (maxvalue < 0.01)
+            {
+                prediction = -1;
+            }
+            else
+            {
+                prediction = _out.IndexOf(_out.Max());
+            }
 
-        int index = _out.FindIndex(x => x > 0.75);
+        }
 
-        Debug.Log($"index : {index}");
+        Debug.Log($"index : {prediction}");
+
+        if (prediction == 1)
+            GameObject.Find("1LeftWink").gameObject.GetComponent<Button>().onClick.Invoke();
+        else if (prediction == 2)
+            GameObject.Find("2RightWink").gameObject.GetComponent<Button>().onClick.Invoke();
+        else if (prediction == 3)
+            GameObject.Find("3BothWink").gameObject.GetComponent<Button>().onClick.Invoke();
+        else if (prediction == 4)
+            GameObject.Find("4Raise").gameObject.GetComponent<Button>().onClick.Invoke();
+        else if (prediction == 5)
+            GameObject.Find("5Lower").gameObject.GetComponent<Button>().onClick.Invoke();
+
     }
 
     // Update is called once per frame
